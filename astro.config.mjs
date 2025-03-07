5ee589cc9a65ae4d753788f92c4455b48bf67bd8
antoine <guingand.antoine@gmail.com>
2025-03-07 11:35:21 +0100

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@
 import { defineConfig } from 'astro/config';
 import alpine from '@astrojs/alpinejs';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [alpine()],
+  integrations: [alpine()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
